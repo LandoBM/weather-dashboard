ba183fe742bd292e0d5bd60a390b496a46532f3f
Landon Murray <landonbmurray@icloud.com>
2022-11-30 23:25:08 -0500

    added some javascript

--- Assets/javascript/script.js
+++ Assets/javascript/script.js
@@ -1,2 +1,11 @@
+// API Key from Open Weather Map
+// var queryURL = "http://api.openweathermap.org/data/2.5/weather?q="
 var APIKey = 'e6c69d7bf2289a95c8c956423a13b208'
-var city
+var city = 'Atlanta'
+
+// Pull the Current Day and Date
+var date = moment().format('MMMM D YYYY, dddd')
+
+
+
+
