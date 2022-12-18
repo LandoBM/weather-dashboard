2db16eeb4c3494d89548ca3b6decb5f4c842194e
Landon Murray <landonbmurray@icloud.com>
2022-12-18 14:43:10 -0500

    added functionanilty to js getWeather

--- Assets/javascript/script.js
+++ Assets/javascript/script.js
@@ -2,10 +2,10 @@
 // var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=e6c69d7bf2289a95c8c956423a13b208"
 var apiKey = 'e6c69d7bf2289a95c8c956423a13b208'
 // var city = 'Atlanta'
-var currentWeather = document.querySelectorAll('.currentWeather')
-var currentDayWeather = document.querySelectorAll('.currentDayBody')
-var futureDayWeather = document.querySelectorAll('.weatherFuture')
-var currentTemp =document.querySelectorAll('.currentTemp')
+// var currentWeather = document.querySelectorAll('.currentWeather')
+// var currentDayWeather = document.querySelectorAll('.currentDayBody')
+// var futureDayWeather = document.querySelectorAll('.weatherFuture')
+// var currentTemp =document.querySelectorAll('.currentTemp')
 
 // Pull the Current Day and Date
 var date = document.querySelectorAll('.currentDayDate')
@@ -36,10 +36,21 @@ function getWeather (city) {
         return response.json()
     })
     .then(function (data){
-        console.log(data)
+       console.log(data)
     })
 
     function displayWeather (data){
-
+        var { name } = data;
+        var { icon, description } = data.getWeather[0];
+        var { temp,feels_like, humidity } = data.main;
+        var { speed } = data.wind;
+        console.log(name,icon,description,temp,feels_like,humidity,speed)
+        document.querySelector('.currentDayCity').innerHTML= name
+        document.querySelector('#icon').src = "https://openweathermap.org/img/wn/"+ icon +"@2x.png"
+        document.querySelector('#ct').innerHTML = temp + '°F'
+        document.querySelector('#cd').innerHTML= description
+        document.querySelector('#cf').innerHTML= feels_like + '°F'
+        document.querySelector('#ch').innerHTML=humidity + '%'
+        document.querySelector('#cw').innerHTML= speed + "MPH"
     }
 }
